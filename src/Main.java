e340df5efee2e8a2b4b3e429c7e38c7904d8801a
jaipradhiksha <pradhikshajai@gmail.com>
2023-08-24 11:05:59 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+/*
+The rules for generating Collatz Sequence are:
+If n is even: n = n / 2 If n is odd: n = 3n + 1
+For example, if the starting number is 5 the sequence is: 5 -> 16 -> 8 -> 4 -> 2 -> 1
+It has been proved for almost all integers, the repeated application of the above rule will result in a sequence that ends at 1.
+The input containing an integer ‘n’ which denotes the given number. Print the numbers in the sequence as output.
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        while (number != 1) {
+            System.out.print(number + " ");
+
+            if (number % 2 == 0) {
+                number /= 2;
+
+            } else {
+                number = 3 * number + 1;
+
+            }
+        }
+        System.out.println("1");
+
+
+    }
+}
+
